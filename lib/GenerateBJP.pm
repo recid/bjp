f2f2b7546bd84362c37457e0e0cb4581b0fcc7ed
davidcoutadeur <david.coutadeur@gmail.com>
2015-03-06 20:42:51 +0100

    find generic method to invoke latexmk with outputdir

--- lib/GenerateBJP.pm
+++ lib/GenerateBJP.pm
@@ -108,11 +108,13 @@ sub displayBJP {
 
   system("rm", "-f", "$script_path/latex/bjp.pdf", "$script_path/latex/bjp.aux", "$script_path/latex/bjp.fls", "$script_path/latex/bjp.log", "$script_path/latex/bjp.fdb_latexmk");
   print STDERR "-outdir=$script_path/latex";
-  system("latexmk", "-pdf", "-cd", "-outdir=$script_path/latex", "latex/bjp.tex");
+  system("cd" "latex");
+  system("latexmk", "-pdf", "-outdir=$script_path/latex", "latex/bjp.tex");
   if ( $? == -1 )
   {
     print STDERR "command failed: $!\n";
   }
+  system("cd" "..");
 
   return "bjp.pdf";
 }
